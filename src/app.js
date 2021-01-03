dd28ea58407072aa2f4f916b207e3cb190880a94
Tanmay <iamtanmaykh07@gmail.com>
2021-01-03 22:35:18 +0530

    new data update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
+const { static } = require("express");
 const express = require("express");
+const path = require("path");
 const app = express();
+require("./db/conn")
+
 const port = process.env.PORT || 8000; 
 
+const static_path = path.join(__dirname , "../public")
+
+app.use(express.static(static_path))
+app.set("view engine" , "hbs")
+
 app.get("/" , (req,res) =>{
-    res.send("Hello there")
+    res.render("index")
 })
 
 app.listen(port, () =>{
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,3 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/registrationform")
+mongoose.connect("mongodb://localhost:27017/registrationform",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+}).then(() =>{
+    console.log(`Connection Successfull`)
+}).catch((err) =>{
+    console.log("No Connection")
+})
